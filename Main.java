47027e97444967e6fce3ee9990e9c28b84b48bfd
aaron-caveney <157995221+aaron-caveney@users.noreply.github.com>
2025-03-13 14:11:35 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package sol;
+
+public class Main {
+    /**
+     * main method
+     * @param args
+     */
+    public static void main(String[] args) {
+        NodeEdgeGraph G = new NodeEdgeGraph("sample");
+    }
+}
